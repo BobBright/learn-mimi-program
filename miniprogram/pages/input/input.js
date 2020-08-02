850459bdc09cb2873d2ccb621975050a86e3709f
moccy <3410431@qq.com>
2020-08-02 11:33:32 +0800

    learned page component

---
+++ miniprogram/pages/input/input.js
@@ -0,0 +1,77 @@
+// pages/input/input.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  handleInput: function (event) {
+    console.log("输入事件")
+  },
+  handleFocus: function (event) {
+    console.log("聚焦事件")
+  },
+  handleBlur: function (event) {
+    console.log("失焦事件")
+  },
+
+})
